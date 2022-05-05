c5bd4ee05b689e1d938d6e00c1d1cb1f7ebf4d51
Chandima Jayasiri <chandimaj@Chandimas-MacBook-Pro.local>
2022-05-05 12:14:51 +0530

    api test

--- restapi/settings.php
+++ restapi/settings.php
@@ -1,13 +1,13 @@
 <?php
 //Import wpdb from wordpress
-/*
-require_once('../../../../wp-load.php');
+
+//require_once('../../../../wp-load.php');
 if (!isset($wpdb)){
     $msg = 'Error loading wpdb';
     echo ($msg);
     die();
 }
-*/
+
 
 //Incoming
 $method = $_POST['method'];

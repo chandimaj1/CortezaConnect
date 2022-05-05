fb841c3a65552e91997aa8bc717880bfe695b6c8
Chandima Jayasiri <chandimaj@Chandimas-MacBook-Pro.local>
2022-05-05 13:58:58 +0530

    token

--- restapi/token.php
+++ restapi/token.php
@@ -19,25 +19,24 @@ echo ($basic_auth);
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => '',
+    CURLOPT_URL => 'https://pyd-sandbox.staging.crust.tech/auth/oauth2/token',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => 'grant_type=client_credentials&scope=profile%20api',
+    CURLOPT_HTTPHEADER => array(
+      'Authorization: Basic MjgxNjAzMjAyNDg5NTE2MDQ4Oms5WmRXbWRxN2E4MmlEWm5HNGhrbHZmVlRlT3hDcjl1S01QR1NBbnNVOFBqNHdNOEpFcDA5aE4xaUlpZWhIQTg=',
+      'Content-Type: application/x-www-form-urlencoded'
+    ),
+  ));
   
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => 'https://pyd-sandbox.staging.crust.tech',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS => 'grant_type=client_credentials&scope=profile%20api',
-  CURLOPT_HTTPHEADER => array(
-    'Authorization: Basic MjgxNjAzMjAyNDg5NTE2MDQ4Oms5WmRXbWRxN2E4MmlEWm5HNGhrbHZmVlRlT3hDcjl1S01QR1NBbnNVOFBqNHdNOEpFcDA5aE4xaUlpZWhIQTg=',
-    'Content-Type: application/x-www-form-urlencoded'
-  ),
-));
-
-$response = curl_exec($curl);
-
-curl_close($curl);
-echo $response;
+  $response = curl_exec($curl);
+  
+  curl_close($curl);
+  echo $response;
 
 var_dump ($response);

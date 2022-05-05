f12f91744b28697080b7b7c46efad8b52425e227
Chandima Jayasiri <chandimaj@Chandimas-MacBook-Pro.local>
2022-05-05 13:36:40 +0530

    token

--- restapi/token.php
+++ restapi/token.php
@@ -11,28 +11,27 @@ if (!isset($wpdb)){
 $table_name = $wpdb->prefix."CortezaConnect_settings";
 $sql = "SELECT DISTINCT * FROM $table_name WHERE id=1";
 $result = $wpdb->get_results( $sql );
+$result = $result[0];
 
-var_dump($result);
+var_dump($result[0]);
 
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  //CURLOPT_URL => $result->cc_instance_url,
-  CURLOPT_URL => "https://pyd-sandbox.staging.crust.tech",
-  CURLOPT_RETURNTRANSFER => true,
-  //CURLOPT_USERPWD => $result->cc_user_id . ":" . $result->$cc_secret,
-  CURLOPT_USERPWD => "281603202489516048:k9ZdWmdq7a82iDZnG4hklvfVTeOxCr9uKMPGSAnsU8Pj4wM8JEp09hN1iIiehHA8",
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS => 'grant_type=client_credentials&scope=profile%20api',
-  CURLOPT_HTTPHEADER => array(
-    'Content-Type: application/x-www-form-urlencoded'
-  ),
-));
+    CURLOPT_URL => 'https://pyd-sandbox.staging.crust.tech/auth/oauth2/token',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => 'grant_type=client_credentials&scope=profile%20api',
+    CURLOPT_HTTPHEADER => array(
+      'Authorization: Basic MjgxNjAzMjAyNDg5NTE2MDQ4Oms5WmRXbWRxN2E4MmlEWm5HNGhrbHZmVlRlT3hDcjl1S01QR1NBbnNVOFBqNHdNOEpFcDA5aE4xaUlpZWhIQTg=',
+      'Content-Type: application/x-www-form-urlencoded'
+    ),
+  ));
 
 $response = curl_exec($curl);
 curl_close($curl);

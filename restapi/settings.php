a9a5cb12a049d10f45495449c711bd7ce36748ff
Chandima Jayasiri <chandimaj@Chandimas-MacBook-Pro.local>
2022-05-05 12:35:05 +0530

    api test

--- restapi/settings.php
+++ restapi/settings.php
@@ -31,14 +31,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $sql = "SELECT DISTINCT * FROM $table_name WHERE id=1";
     $result = $wpdb->get_results( $sql );
     $response = $result;
+    $msg = "success";
 }
 
 //POST
 else if (
     $_SERVER['REQUEST_METHOD'] === 'POST' 
     && isset($data) && !empty($data)
-    && isset($data["cc_user_id"]) && !is_null($data["cc_user_id"]) 
-    && isset($data["cc_secret"]) && !is_null($data["cc_secret"]) ) {
+    ) {
     $data["id"]=1; //Set row id as 1
     $update_db = $wpdb->replace($table_name, $data);
     if($update_db){ 

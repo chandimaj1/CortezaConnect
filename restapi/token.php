99dbc9ebe8733ad8a48f9222daa4be9e2325cdb3
Chandima Jayasiri <chandimaj@Chandimas-MacBook-Pro.local>
2022-05-05 13:40:57 +0530

    token

--- restapi/token.php
+++ restapi/token.php
@@ -19,17 +19,19 @@ $curl = curl_init();
 
 curl_setopt_array($curl, array(
   CURLOPT_URL => 'https://pyd-sandbox.staging.crust.tech/auth/oauth2/token',
+
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => '',
   CURLOPT_MAXREDIRS => 10,
+  CURLOPT_USERPWD =>  "281603202489516048:k9ZdWmdq7a82iDZnG4hklvfVTeOxCr9uKMPGSAnsU8Pj4wM8JEp09hN1iIiehHA8",
   CURLOPT_TIMEOUT => 0,
   CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => 'POST',
   CURLOPT_POSTFIELDS => 'grant_type=client_credentials&scope=profile%20api',
   CURLOPT_HTTPHEADER => array(
-    'Authorization: Basic MjgxNjAzMjAyNDg5NTE2MDQ4Oms5WmRXbWRxN2E4MmlEWm5HNGhrbHZmVlRlT3hDcjl1S01QR1NBbnNVOFBqNHdNOEpFcDA5aE4xaUlpZWhIQTg=',
-    'Content-Type: application/x-www-form-urlencoded'
+    //'Authorization: Basic MjgxNjAzMjAyNDg5NTE2MDQ4Oms5WmRXbWRxN2E4MmlEWm5HNGhrbHZmVlRlT3hDcjl1S01QR1NBbnNVOFBqNHdNOEpFcDA5aE4xaUlpZWhIQTg=',
+    'Content-Type: application/xml'
   ),
 ));
 

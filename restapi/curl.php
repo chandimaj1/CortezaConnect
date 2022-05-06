8855abf5b56d78677b5bdd16047af6058320f001
Chandima Jayasiri <chandimaj@Chandimas-MacBook-Pro.local>
2022-05-06 14:39:08 +0530

    token

--- restapi/curl.php
+++ restapi/curl.php
@@ -10,6 +10,8 @@ if (!isset($wpdb)){
 $params = $params->get_json_params();
 $data = $params["data"];
 
+var_dump($data);
+
 $token = $data["cc_token"];
 $endpoint = $data["endpoint"];
 $method = $data["method"];
@@ -22,30 +24,26 @@ $sql = "SELECT DISTINCT cc_instance_url, cc_token FROM $table_name WHERE id=1";
 $result = $wpdb->get_results( $sql );
 $result = $result[0];
 
-$instance = $result->cc_instance_url;
+$url = $result->cc_instance_url.$endpoint;
 $basic_auth = 'Authorization: Bearer '.$result->cc_token;
-
-echo ($basic_auth);
 
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-    CURLOPT_URL => $instance.$endpoint,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => $method,
-    CURLOPT_POSTFIELDS => 'grant_type=client_credentials&scope=profile%20api',
-    CURLOPT_HTTPHEADER => array(
-      $basic_auth,
-      'Content-Type: application/x-www-form-urlencoded'
-    ),
-  ));
-  
-  $response = curl_exec($curl);
+  CURLOPT_URL => $url,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => $method,
+  CURLOPT_HTTPHEADER => array(
+    $basic_auth
+  ),
+));
+
+$response = curl_exec($curl);
   curl_close($curl);
 
 $msg = "Error getting token";
